84a36ef8ecb7ec095532d93c6f180cc37bfd2bd2
qfcwell <qfcqfcqfc@qq.com>
2017-10-21 09:36:33 +0800

    1

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = '2535199139@qq.com'
+    MAIL_DEFAULT_SENDER = 'flask@minitech.site'
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flask@minitech.site>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
